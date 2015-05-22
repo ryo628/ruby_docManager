6457843cc219cd576b9984628fbd25e9e8c18429
shinob <shinob@mgg.nifty.ne.jp>
2015-05-23 04:51:27 +0900

    add setup script.

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# パスワードファイルの作成
+
+htpasswd -c -b .htpasswd admin admin
+htpasswd -b .htpasswd user user
+htpasswd -b .htpasswd guest guest
+
+mv .htpasswd /var/www/
+
+# ファイル属性の変更
+
+chmod 755 docManager/*.rb
+chmod 777 docManager/files
+chmod 777 docManager/lib/databases
+chmod 777 docManager/lib/databases/docManager.db
+
